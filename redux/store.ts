4e205e114681602a00f38c27cbefc67167642385
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-04-21 21:21:06 +0600

    added new logic

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
+import counterReducer from './features/counter/counterSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    counter: counterReducer,
   },
 });
 
